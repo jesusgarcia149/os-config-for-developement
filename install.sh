8073a56258996c666c4c0e4be53073f61ab8062a
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-08-08 21:31:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,81 @@
-# Google Chrome
+#! /bin/bash
+
+sudo apt update
 
 sudo apt install wget -y
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt update
 sudo apt install google-chrome-stable -y
+
+sudo apt install firefox -y
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+sudo rm microsoft.gpg
+sudo apt update
+sudo apt install microsoft-edge-stable -y
+
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https -y
+sudo apt update
+sudo apt install code -y
+
+sudo apt install git -y
+sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.1.1-linux1/GitHubDesktop-linux-3.1.1-linux1.deb
+### Uncomment below line if you have not installed gdebi-core before
+# sudo apt-get install gdebi-core 
+sudo gdebi GitHubDesktop-linux-3.1.1-linux1.deb
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt install virtualbox -y
+
+sudo apt install openjdk-11-jdk -y
+java --version
+sudo add-apt-repository ppa:maarten-fonville/android-studio -y
+sudo apt update
+sudo apt install android-studio -y
+
+sudo apt install nodejs -y
+wget https://downloads.mongodb.com/compass/mongodb-compass_1.39.0_amd64.deb
+sudo dpkg -i mongodb-compass_1.39.0_amd64.deb
+
+sudo apt-get update
+sudo apt-get install mysql-server mysql-client libmysqlclient-dev -y
+sudo apt-get install apache2 apache2-doc apache2-npm-prefork apache2-utils libexpat1 ssl-cert -y
+sudo apt-get install libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-gd php-pear php-imagick php7.0-mcrypt php7.0-mysql php7.0-ps php7.0-xsl -y
+sudo apt-get install phpmyadmin -y
+
+sudo apt-get update
+sudo apt-get install curl unzip -y
+sudo apt-get install php php-curl -y
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+sudo composer self-update 
+
+sudo apt install htop -y
+sudo apt install screenfetch -y
+
+sudo snap install teams -y
+sudo snap install slack --classic -y
+
+cd /tmp/
+wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_2.0.0.zip
+# sha1sum: 7b213dcf0bb4ac20d17007f52192c7914b10ed3f
+unzip droidcam_latest.zip -d droidcam
+cd droidcam && sudo ./install-client
