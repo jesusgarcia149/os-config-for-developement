02de145ebca7291581115f22543330f3212ebc21
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-08-08 21:13:32 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+# Google Chrome
+
+sudo apt install wget -y
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt update
+sudo apt install google-chrome-stable -y
